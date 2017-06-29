8d8688f0969bc8d1acfc5f71a73bc6ae5c17e199
ThomasGuigard <thomasguigard@gmail.com>
2017-06-29 17:12:33 +0200

    ajout de la classe operation et refactorisation de calculator aus sein de cette classe

--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -1,93 +1,12 @@
-﻿using System;
-using System.CodeDom;
-using System.Linq;
-using System.Net.NetworkInformation;
-
-namespace SolidExercices
+﻿namespace SolidExercices
 {
     public class Calculator
     {
         public decimal Calculate(string operation)
         {
             decimal result = 0;
-            char[] operators = {'+', '-', 'x', '/'};
-            int ct = 1;
-            var error = "Opération impossible";
-            String[] sousOperations;
-
-
-            if (String.IsNullOrEmpty(operation) || !operators.Any(operation.Contains))
-            {
-                throw new ArgumentException(error);
-            }
-
-            foreach (var myOperator in operators)
-            {
-              
-                if (!operation.Contains(myOperator))
-                {
-                    continue;  
-                }
-
-                sousOperations = operation.Split(myOperator);
-
-                try
-                {
-                    result = Convert.ToDecimal(sousOperations[0]);
-                }
-                catch (Exception e)
-                {
-                    error = "Valeur incorrect : " + sousOperations[0];
-                    throw new ArgumentException(error);
-                }
-                
-
-                for (int i = 1; i < sousOperations.Length; i++)
-                {
-                    decimal chiffre;
-
-                    try
-                    {
-                        chiffre = Convert.ToDecimal(sousOperations[i]);
-                    }
-                    catch (Exception e)
-                    {
-                        error = "Valeur incorrect : " + sousOperations[i];
-                        throw new ArgumentException(error);
-                    }
-
-                    switch (myOperator)
-                    {
-                        case '+':
-                            result = result + chiffre;
-                            break;
-
-                        case '-':
-                            result = result - chiffre;
-                            break;
-
-                        case 'x':
-                            result = result * chiffre;
-                            break;
-
-                        case '/':
-                            try
-                            {
-                                result = result / chiffre;
-                            }
-                            catch (DivideByZeroException)
-                            {
-                                error = "La division par 0 est impossible ! Rentrez une autre valeur..";
-                                throw new ArgumentException(error);
-                            }
-                            break;
-                    }
-                }
-
-                
-            }
-
-
+            Operation operators =  new Operation(operation);
+            operators.FindAnswer();
             return result;
         }
     }
---
+++ SolidExercices/Operation.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+
+namespace SolidExercices
+{
+    class Operation
+    {
+        public char[] Type { get; set; }
+        public string[] SousOperations { get; set; }
+
+        public string MyOperation { get; set; }
+
+        public Operation(String myOperation)
+        {
+            MyOperation = myOperation;
+            Type = new []{'+', '-', 'x', '/'};
+        }
+
+        public decimal FindAnswer()
+        {
+            GotOperators(MyOperation);
+
+            decimal finalResult = 0;
+
+            foreach (var myOperator in Type)
+            {
+
+                if (!MyOperation.Contains(myOperator))
+                {
+                    continue;
+                }
+
+                SousOperations = MyOperation.Split(myOperator);
+
+                try
+                {
+                    finalResult = Convert.ToDecimal(SousOperations[0]);
+                }
+                catch (Exception)
+                {
+                    GetError(3);
+                }
+
+                Calcul(myOperator);
+
+            }
+
+            return finalResult;
+        }
+
+        public decimal Calcul(char myOperator)
+        {
+            decimal result = 0;
+
+            for (int i = 1; i < SousOperations.Length; i++)
+            {
+                decimal chiffre = 0;
+
+                try
+                {
+                    chiffre = Convert.ToDecimal(SousOperations[i]);
+                }
+                catch (Exception)
+                {
+                    GetError(3);
+                }
+
+                switch (myOperator)
+                {
+                    case '+':
+                        result = result + chiffre;
+                        break;
+
+                    case '-':
+                        result = result - chiffre;
+                        break;
+
+                    case 'x':
+                        result = result * chiffre;
+                        break;
+
+                    case '/':
+                        try
+                        {
+                            result = result / chiffre;
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            GetError(2);
+                        }
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        public void GotOperators(String operation)
+        {
+            if (String.IsNullOrEmpty(operation) || !Type.Any(operation.Contains))
+            {
+                GetError(1);
+            }
+
+        }
+
+        public void GetError(int type)
+        {
+            String error;
+            switch (type)
+            {
+                case 1:
+                    error = "Opération impossible";
+                    break;
+                case 2:
+                    error = "La division par 0 est impossible ! Rentrez une autre valeur..";
+                    break;
+                default:
+                    error = "Defaut inconnnu";
+                    break;
+            }
+            throw new ArgumentException(error);
+        }
+    }
+}
