1ce15919cbeea15092251892ff0978a60457117f
ThomasGuigard <thomasguigard@gmail.com>
2017-06-29 14:14:41 +0200

    Ajout de tests basiques sur les calculs

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -12,5 +12,45 @@ namespace SolidExercices.Tests
             var result = calculator.Calculate("1+2,3");
             Check.That(result).IsEqualTo(3.3);
         }
+
+        [Test]
+        public void CalculateANegSum()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1-2,3");
+            Check.That(result).IsEqualTo(3.3);
+        }
+
+        [Test]
+        public void CalculateAProduct()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1+2,3");
+            Check.That(result).IsEqualTo(3.3);
+        }
+
+        [Test]
+        public void CalculateAPosSubstraction()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1+2,3");
+            Check.That(result).IsEqualTo(3.3);
+        }
+
+        [Test]
+        public void CalculateANegSubstraction()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1-9,3");
+            Check.That(result).IsEqualTo(3.3);
+        }
+
+        [Test]
+        public void CalculateADivWithZero()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1+2,0");
+            Check.That(result).IsEqualTo(3.3);
+        }
     }
 }
